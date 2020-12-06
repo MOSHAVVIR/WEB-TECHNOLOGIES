1b2f8db15f1c255896005f86f47599b0d50ae2b0
MOSHAVVIR <57568733+MOSHAVVIR@users.noreply.github.com>
2020-12-06 20:34:17 +0600

    Add files via upload

---
+++ TASK5-FINAL TERM.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lab Task 4</title>
+	<style>
+		h1{color: white;}
+		h4{color: white;}
+		p{color: white;}
+		button{color: white;}
+		body{padding: 0; margin:0 ;border:0;}
+		.box{
+			width: 1346px;
+			height: 500px;
+			text-align: center;
+			padding: 0px;
+			margin: 0px;
+			border: 3px auto;
+			background-image:url(a.jpg); 
+		}
+		.box1{
+			width: 1346px;
+			height: 30px;
+			text-align: center;
+			padding: 0px;
+			margin: 0px;
+			border: 3px auto;
+			background-color: white; 
+		}
+		.box2{
+			width: 1346px;
+			height: 450px;
+			text-align: center;
+			padding: 0px;
+			margin: 0px;
+			border: 3px auto;
+			background-color: grey; 
+		}
+		.box3{
+			width: 250px;
+			height: 400px;
+			text-align: center;
+			padding:  10px;
+			margin: 250px;
+			border: 3px auto;
+			background-color: white; 
+		}
+		.box4{
+			width: 250px;
+			height: 400px;
+			text-align: center;
+			padding: 10px;
+			margin: 550px;
+			border: 3px auto;
+			background-color: white; 
+		}
+		.box5{
+			width: 250px;
+			height: 400px;
+			text-align: center;
+			padding: 10px;
+			margin: 550px;
+			border: 3px auto;
+			background-color: white; 
+		}
+		.box6{
+			width: 1346px;
+			height: 50px;
+			text-align: center;
+			padding: 0px;
+			margin: 0px;
+			border: 3px auto;
+			background-color: white; 
+		}
+		.box7{
+			width: 673px;
+			height:250px;
+			text-align: center;
+			padding: 0px;
+			margin: 0px;
+			border: 3px auto;
+			background-color: blue; 
+		}
+		.box8{
+			width: 673px;
+			height:250px;
+			text-align: center;
+			padding: 0px;
+			margin-right: 673px;
+			border: 3px auto;
+			background-color: grey; 
+		}
+		.box9{
+			width: 1346px;
+			height: 250px;
+			text-align: center;
+			padding: 0px;
+			margin: 0px;
+			border: 3px auto;
+			background-color: black; 
+		}
+		.btn{
+			background: none;
+			font-size: 20px;
+			padding: 10px 20px;
+			margin: 10px;
+		}
+		.mainDiv{
+			margin-left: 5%;
+			margin-right: 5%;
+		}
+		.mainDivdiv{
+			margin-left: 5%;
+			margin-right: 5%;
+			display: inline-block;
+			height: 200px;
+			width: 300px;
+		}
+		.mainDiv1{
+			margin-left: 0%;
+			margin-right: 5%;
+		}
+		.mainDivdiv1{
+			margin-left: 0%;
+			margin-right: 25%;
+			display: inline-block;
+			height: 200px;
+			width: 300px;
+		}
+		.mainDiv2{
+			margin-left: 5%;
+			margin-right: 5%;
+		}
+		.mainDivdiv2{
+			margin-left: 5%;
+			margin-right: 5%;
+			display: inline-block;
+			height: 200px;
+			width: 300px;
+		}
+	</style>
+</head>
+<body>
+	<div class="box">
+		<br><br>
+		<h1><Font size="16"> Welcome to Acme Web Solutions</Font></h1>
+		<br>
+		<p><Font size="6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci eum error earum soluta voluptatum nisi laboriosam eos saepe asperiores dolorum.</Font></p>
+		<div class="btn">
+			<button class="btn btn1">Read More</button>
+		</div>
+	</div>
+	<div class="box1">
+		<h2><Font size="6"> Web & Application Development</Font></h2>
+		<div class="mainDiv">
+			<div class="mainDivdiv">
+				
+				<p2><Font size="5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe sint eligendi possimus? Unde officiis magnam laborum ipsa distinctio odio, vero dolores
+				</Font> </p2>
+			</div>
+
+			<div class="mainDivdiv">
+				<p2><Font size="5">dicta aliquam aperiam repellendus. Perferendis officiis deserunt velit voluptas nobis sequi animi totam, accusantium, ex eius quia, natus quo?
+				</Font></p2>
+			</div>
+		</div>	
+	</div>
+
+	<div class="box2">
+		<div class="box3">
+		
+			
+		</div>
+
+	</div>
+	<div class="box6">
+		<h3><Font size="6"> We handle all of your digital needs </Font> </h3>
+		<br>
+		<p3><Font size="4"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime nam rerum vel earum error fugiat cupiditate, dolore eius! Minus, explicabo.</p3>
+		
+	</div>
+	<br><br><br>
+
+	<div class="mainDiv1">
+		<div class="mainDivdiv1">
+	<div class="box7">
+		<br><br>
+		<h4><Font size="6">Contact Us</Font> </h4>
+		
+		<p><Font size="4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, suscipit. Rerum ducimus a quod, ut et voluptas obcaecati unde fuga.
+		<br><br>
+		contact@acmewebsolutions.test
+		</p>
+		
+	</div>
+	</div>
+	
+<div class="mainDivdiv1">
+	<div class="box8">
+		<br><br>
+		<h4><Font size="6">About Our Company</Font> </h4>
+		
+		<p><Font size="4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio earum porro deserunt, deleniti, quae facere repudiandae, officiis est exercitationem nobis iusto doloremque! Soluta excepturi in aut suscipit amet temporibus quo?
+		
+		</p>
+		
+	</div>
+	</div>
+	</div>
+
+		
+	<div class="box9">
+		<br><br><br><br>
+		<div class="mainDiv2">
+			<div class="mainDivdiv2">
+				
+				<p><Font size="5">Acme Web Solutions
+				</Font> </p>
+			</div>
+
+			<div class="mainDivdiv2">
+				<p><Font size="5">Project By Traversy Media
+				</Font></p>
+			</div>
+		</div>	
+	</div>
+		
+	</div>
+
+
+
+</body>
+</html>
